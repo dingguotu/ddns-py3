ed6e1044b2ec13e3caba23d9a5fcac03e622a975
tinko <1335162473@qq.com>
2019-11-29 17:32:30 +0800

    first commit

---
+++ ddns.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import socket 
+import json
+import os
+import dnspod
+
+global LocalIP
+global HostIP
+global Login_Token
+global Domain_Id
+
+def init_domain(domain):
+    global Domain_Id
+
+    domain_exists = check_domain_exists(domain)
+    if domain_exists == False:
+        Domain_Id = dnspod.create_domain(Login_Token, domain['name'])
+
+
+def check_domain_exists(domain):
+    global Domain_Id
+
+    Domain_Id = dnspod.get_domain_id(Login_Token, domain['name'])
+    if Domain_Id == 0:
+        return False
+    else:
+        return True
+
+
+def ddns(domain):
+    global Domain_Id
+
+    for sub_domain in domain['sub_domains']:
+        record_id = dnspod.get_record_id(Login_Token, Domain_Id, sub_domain)
+        if record_id == 0:
+            dnspod.create_record_id(Login_Token, Domain_Id, sub_domain, LocalIP)
+        else:
+            dnspod.record_ddns(Login_Token, Domain_Id, record_id, sub_domain, LocalIP)
+
+    
+def get_ip():
+    global LocalIP
+    sock = socket.create_connection(('ns1.dnspod.net', 6666), 20)
+    LocalIP = sock.recv(16)
+    print(LocalIP)
+    sock.close()
+
+
+def get_host_ip(domain):
+    global HostIP
+    global Domain_Id
+    HostIP = dnspod.get_record_value(Login_Token, Domain_Id, domain['sub_domains'][0])
+    print(HostIP)
+
+    
+if __name__ == '__main__':
+    global Login_Token
+    conf = json.load(open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "conf.json"), "r"))
+
+    Login_Token = "%s,%s" % (conf['id'], conf['token'])
+    Domains = conf['domains']
+    
+    try:
+        get_ip()
+        for domain in Domains:
+            init_domain(domain)
+            get_host_ip(domain)
+            if HostIP != LocalIP:
+                print(f"Begin update [{domain['name']}].")
+                ddns(domain)
+    except Exception as e:
+        print(e)
+        pass
---
+++ dnspod.py
@@ -0,0 +1,131 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+from urllib import request, parse
+import json
+import os
+
+Headers = {
+    'Accept': 'text/json',
+    'Content-type': 'application/x-www-form-urlencoded',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    }
+
+def getRequest(api, data):
+    return request.Request(url=f'https://dnsapi.cn/{api}', data=parse.urlencode(data).encode('utf-8'), headers=Headers, method='POST')
+
+
+def get_domain_id(login_token, domain_name):
+    params = dict(
+        login_token=login_token,
+        format="json"
+    )
+    req = getRequest('Domain.List', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        domails = data['domains']
+        for domain in domails:
+            if domain['name'] == domain_name:
+                return domain['id']
+        return 0
+    else:
+        return 0
+
+
+def create_domain(login_token, domain_name):
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain = domain_name
+    )
+    req = getRequest('Domain.Create', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        return data['domain']['id']
+    else:
+        return 0
+
+
+def get_record_value(login_token, domain_id, sub_domain):
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        sub_domain = sub_domain
+    )
+    req = getRequest('Record.List', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        records = data['records']
+        for record in records:
+            if record['type'] == 'A' and record['name'] == sub_domain:
+                return record['value']
+        return "127.0.0.1"
+    else:
+        return "127.0.0.1"
+
+
+def get_record_id(login_token, domain_id, sub_domain):
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        sub_domain = sub_domain
+    )
+    req = getRequest('Record.List', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        records = data['records']
+        for record in records:
+            if record['type'] == 'A' and record['name'] == sub_domain:
+                return record['id']
+        return 0
+    else:
+        return 0
+
+
+def create_record_id(login_token, domain_id, sub_domain, localIP):
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        sub_domain = sub_domain,
+        record_type = 'A',
+        record_line_id = "0",
+        value = localIP
+    )
+    req = getRequest('Record.Create', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        print (f"Sub_domain [{sub_domain}] create success")
+
+
+def record_ddns(login_token, domain_id, record_id, sub_domain, localIP):
+    req = getRequest('Record.Ddns')
+    params = dict(
+        login_token=login_token,
+        format="json",
+        domain_id = domain_id,
+        record_id = record_id,
+        sub_domain = sub_domain,
+        record_line_id = "0",
+        value = localIP
+    )
+
+    req = getRequest('Record.Ddns', params)
+    response = request.urlopen(req)
+    data = json.loads(response.read().decode('utf-8'))
+
+    if int(data['status']['code']) == 1:
+        print (f"DDns Success for subdomain [{sub_domain}], IP change to {localIP}")
+    else:
+        print (f"DDns Error for subdomain [{sub_domain}]: {data['status']['message']}")
